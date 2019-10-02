baf63294ec8f878c5bda1610352ef018febdd7d3
linazuniga <55807176+linazuniga@users.noreply.github.com>
2019-10-02 13:11:03 -0500

    versioni 2.0

--- src/gimnasio/Gimnasio.java
+++ src/gimnasio/Gimnasio.java
@@ -9,5 +9,53 @@ public class Gimnasio {
     private String colorDeLasMaquinas;
     private boolean fisioterapeutas;
     private int numeroDeEntrenadores;
-    
+
+    public Gimnasio(int numeroDeMaquinas, Date fechasDeInscripcion, String colorDeLasMaquinas, boolean fisioterapeutas, int numeroDeEntrenadores) {
+        this.numeroDeMaquinas = numeroDeMaquinas;
+        this.fechasDeInscripcion = fechasDeInscripcion;
+        this.colorDeLasMaquinas = colorDeLasMaquinas;
+        this.fisioterapeutas = fisioterapeutas;
+        this.numeroDeEntrenadores = numeroDeEntrenadores;
+    }
+
+    public int getNumeroDeMaquinas() {
+        return numeroDeMaquinas;
+    }
+
+    public void setNumeroDeMaquinas(int numeroDeMaquinas) {
+        this.numeroDeMaquinas = numeroDeMaquinas;
+    }
+
+    public Date getFechasDeInscripcion() {
+        return fechasDeInscripcion;
+    }
+
+    public void setFechasDeInscripcion(Date fechasDeInscripcion) {
+        this.fechasDeInscripcion = fechasDeInscripcion;
+    }
+
+    public String getColorDeLasMaquinas() {
+        return colorDeLasMaquinas;
+    }
+
+    public void setColorDeLasMaquinas(String colorDeLasMaquinas) {
+        this.colorDeLasMaquinas = colorDeLasMaquinas;
+    }
+
+    public boolean isFisioterapeutas() {
+        return fisioterapeutas;
+    }
+
+    public void setFisioterapeutas(boolean fisioterapeutas) {
+        this.fisioterapeutas = fisioterapeutas;
+    }
+
+    public int getNumeroDeEntrenadores() {
+        return numeroDeEntrenadores;
+    }
+
+    public void setNumeroDeEntrenadores(int numeroDeEntrenadores) {
+        this.numeroDeEntrenadores = numeroDeEntrenadores;
+    }
+
 }
