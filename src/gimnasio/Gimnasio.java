ec7f5359db6b53d8c8106cb281d47218765b616f
linazuniga <55807176+linazuniga@users.noreply.github.com>
2019-10-02 12:22:30 -0500

    Versioin 1.0

---
+++ src/gimnasio/Gimnasio.java
@@ -0,0 +1,13 @@
+package gimnasio;
+
+import java.util.Date;
+
+public class Gimnasio {
+
+    private int numeroDeMaquinas;
+    private Date fechasDeInscripcion;
+    private String colorDeLasMaquinas;
+    private boolean fisioterapeutas;
+    private int numeroDeEntrenadores;
+    
+}
